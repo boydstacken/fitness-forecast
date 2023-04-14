e27d91eee8862c195f476c1e720290f8106bc782
Abi L <124858748+lailiel@users.noreply.github.com>
2023-04-13 21:23:03 -0500

    incorporated git pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-var muscle = "biceps";
-fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+var muscle = [["Abdominals", "lower_back", "middle_back"],["abductors", "Adductors", "Quadriceps"]
+,["Biceps", "triceps", "forearms"],["Chest", "traps", "Lats"],["glutes", "Hamstrings", "calves"]]
+
+var randomEx1 = Math.floor(Math.random() * 10);
+var randomEx2 = 0;
+
+var exeDay = 1;
+
+function generateNumber() {
+  randomEx2 = Math.floor(Math.random() * 10);
+  if (randomEx1 !== randomEx2) {
+    return randomEx2;
+  } else {
+    generateNumber();
+  }
+}
+
+for (i=0; i<3; i++) {
+fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle[exeDay][i]}`, {
   method: "GET",
   headers: { "X-Api-Key": "JTo+3b4INS07H1+MuR5ygw==xgXVzkHdaEOHWO1Y" },
   contentType: "application/json",
@@ -7,10 +24,11 @@ fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
   if (!response.ok) {
     return response.json();
   }
-  response.json().then(function (data) {
-    console.log(data);
-  });
-});
+  response.json([0]).then(function (data) {
+    console.log(data[randomEx1])
+     console.log(data[randomEx2])}
+  );
+})};
 
 function generateForecastCards() {
   let cityName = "Madison";
