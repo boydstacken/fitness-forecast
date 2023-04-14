5818d74195fb984e6bdef103fc2980db93a69e71
Abi L <124858748+lailiel@users.noreply.github.com>
2023-04-13 20:10:00 -0500

    3 random excersises are added, one from each muscle group, in a given day group

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-var muscle = "biceps";
-fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+var muscle = ["day1", "day2", "day3", "day4", "day5"];
+
+var day1 = ["Abdominals", "lower_back", "middle_back"]
+var day2 = ["abductors", "Adductors", "Quadriceps"]
+var day3 = ["Biceps", "triceps", "forearms"]
+var day4 = ["Chest", "traps", "Lats"]
+var day5 = ["glutes", "Hamstrings", "calves"]
+
+var randomEx = Math.floor(Math.random() * 10);
+
+for (i=0; i<3; i++) {
+fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${day1[i]}`, {
   method: "GET",
   headers: { "X-Api-Key": "JTo+3b4INS07H1+MuR5ygw==xgXVzkHdaEOHWO1Y" },
   contentType: "application/json",
@@ -7,10 +17,12 @@ fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
   if (!response.ok) {
     return response.json();
   }
-  response.json().then(function (data) {
-    console.log(data);
+  response.json([0]).then(function (data) {
+    console.log(data[randomEx])
+    
   });
-});
+})};
+
 
 let cityName = "Madison";
 const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=acbf659b6dad995f4221a78b638e6923`;
@@ -22,3 +34,4 @@ fetch(weatherUrl).then(function (response) {
     console.log(data);
   });
 });
+
