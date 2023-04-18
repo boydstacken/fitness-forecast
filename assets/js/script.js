312bbe2b083e19909ac9f7605766b58e38a8d30c
Jayson Baker <bakej07@gmail.com>
2023-04-17 20:58:23 -0500

    Added Random Cardio selection to all cards on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,18 @@ var muscle = [
 ];
 
 var cardio = [
- ["Dance Fitness", "Spin Class", "Indoor Rock Climbing"],
- ["Swimming", "Kayaking", "Canoeing", "Paddle Boarding"],
- ["Biking", "Yoga", "Roller Skating", "Yard Work"],
- ["Running", "Jogging", "Hiking", "Jump Rope"],
- ["Ice Skating", "Skiing", "Snowboarding", "Snow Shoeing", "Cross Country Skiing"] 
-]
+  ["Dance Fitness", "Spin Class", "Indoor Rock Climbing"],
+  ["Swimming", "Kayaking", "Canoeing", "Paddle Boarding"],
+  ["Biking", "Yoga", "Roller Skating", "Yard Work"],
+  ["Running", "Jogging", "Hiking", "Jump Rope"],
+  [
+    "Ice Skating",
+    "Skiing",
+    "Snowboarding",
+    "Snow Shoeing",
+    "Cross Country Skiing",
+  ],
+];
 
 var randomEx1 = Math.floor(Math.random() * 10);
 var randomEx2 = 0;
@@ -51,14 +57,13 @@ function generateWorkout() {
         return response.json();
       }
 
-  response.json([0]).then(function (data) {
-    console.log(data[randomEx1])
-     console.log(data[randomEx2])}
-     
-  );
-})}
-};
-
+      response.json([0]).then(function (data) {
+        console.log(data[randomEx1]);
+        console.log(data[randomEx2]);
+      });
+    });
+  }
+}
 
 // -------------------------------------------------------------------
 mainWeatherCard = $("#mainWeatherCard");
@@ -73,7 +78,6 @@ function generateForecastCards() {
       return response.json();
     }
     response.json().then(function (data) {
-      console.log(data);
       let fiveDayData = [];
       console.log(fiveDayData);
       data.list.forEach((element) => {
@@ -91,14 +95,35 @@ function generateForecastCards() {
       for (let i = 0; i < fiveDayData.length; i++) {
         let temp = `Temp: ${fiveDayData[i].main.temp.toFixed(2)}°F`;
         //
-        let wind = `Wind: ${fiveDayData[i].wind.speed}mph`;
-        //
         let humidity = `Humidity: ${fiveDayData[i].main.humidity}%`;
         //
         let date = fiveDayData[i].dt_txt.split(" ")[0].replaceAll("-", "/");
         //
         let weatherIcon = fiveDayData[i].weather[0].icon;
         //
+        let cardioWorkout = "";
+        //
+        let weatherCondition = fiveDayData[i].weather[0].main;
+        let tempCompare = fiveDayData[i].main.temp.toFixed(2);
+        console.log(weatherCondition);
+        console.log(tempCompare);
+        if (weatherCondition === "Rain" || tempCompare <= 31) {
+          cardioWorkout =
+            cardio[0][Math.floor(Math.random() * cardio[0].length)];
+        } else if (weatherCondition === "Snow") {
+          cardioWorkout =
+            cardio[4][Math.floor(Math.random() * cardio[4].length)];
+        } else if (tempCompare <= 50) {
+          cardioWorkout =
+            cardio[3][Math.floor(Math.random() * cardio[3].length)];
+        } else if (tempCompare <= 84) {
+          cardioWorkout =
+            cardio[2][Math.floor(Math.random() * cardio[2].length)];
+        } else if (tempCompare > 84) {
+          cardioWorkout =
+            cardio[1][Math.floor(Math.random() * cardio[1].length)];
+        }
+
         if (i === 0) {
           //
           let mainTitle = $("<h1>");
@@ -118,13 +143,13 @@ function generateForecastCards() {
           mainTemp.text(`${temp}`);
           mainWeatherCard.append(mainTemp);
           //
-          let mainWind = $("<p>");
-          mainWind.text(`${wind}`);
-          mainWeatherCard.append(mainWind);
-          //
           let mainHumidity = $("<p>");
           mainHumidity.text(`${humidity}`);
           mainWeatherCard.append(mainHumidity);
+          //
+          let mainCardio = $("<p>");
+          mainCardio.text("Cardio: " + cardioWorkout);
+          mainWeatherCard.append(mainCardio);
         } else {
           let card = $("<div>");
           card.addClass("container bg-blue-600 w-80 h-60 text-2xl dayCastCard");
@@ -152,15 +177,14 @@ function generateForecastCards() {
           cardTemp.text(temp);
           cardBody.append(cardTemp);
           //
-          let cardWind = $("<p>");
-          cardWind.addClass("card-text");
-          cardWind.text(wind);
-          cardBody.append(cardWind);
-          //
           let cardHumidity = $("<p>");
           cardHumidity.addClass("card-text");
           cardHumidity.text(humidity);
           cardBody.append(cardHumidity);
+          //
+          let cardCardio = $("<p>");
+          cardCardio.text("Cardio: " + cardioWorkout);
+          cardBody.append(cardCardio);
           //
         }
       }
